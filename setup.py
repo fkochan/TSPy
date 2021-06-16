118f45d698025615d51536ccfbbfba58637b31f9
ferhatkochan <ferhat@Ferhats-MacBook-Pro.local>
2021-06-15 23:53:07 -0400

    first commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = "TSPy",
+    version = "0.1",
+    packages = find_packages(),
+    install_requires = [],
+    author = "fkochan",
+    description = "A Python wrapper for the TradeStation WebAPI",
+    url = "https://github.com/fkochan/TSPy"
+)
